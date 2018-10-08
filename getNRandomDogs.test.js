de3eb1be36ef6e83a2e0c854aba845af38b9f315
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-08 22:49:44 +0900

    犬を全数テストするように修正 lengthとrandomIntの比較をする処理を修正 もう一点の指摘事項に関してですが、toBe(randomInt)にするとテストとおらない  => メインのロジック修正が必要？？

--- getNRandomDogs.test.js
+++ getNRandomDogs.test.js
@@ -6,10 +6,6 @@ const isDog = function(dog) {
 
 const randomInt =  Math.floor(Math.random() * (11 - 1) + 1);
 
-const measureLength = function(dogs) {
-  return dogs.length === randomInt;
-}
-
 describe('getNRandomDogs', () => {
   test('should take a callback as an argument and call it once', (done) => {
     const runAssertion = function() {
@@ -21,8 +17,10 @@ describe('getNRandomDogs', () => {
   });
   test('should call the callback with N dogs as an argument', (done) => {
     getNRandomDogs(randomInt, function(dogs) {
-      expect(isDog(dogs[0])).toBe(true);
-      expect(measureLength(dogs)).toBe(true);
+      for (let loop = 0; loop < randomInt; loop+=1) {
+        expect(isDog(dogs[loop])).toBe(true);
+      }
+      expect(dogs.length).toBe(randomInt);
       done();
     });
   });
